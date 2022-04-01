f8fd1d8a8394eeb04ca5e4f3b5a3ba9185ba9236
Keisuke <change.1209k.9@gmail.com>
2022-04-01 20:41:09 +0900

    ファイル作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+  
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+end
