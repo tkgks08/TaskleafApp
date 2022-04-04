3085041403d9f2576095915514c37402ee840f2a
Keisuke <change.1209k.9@gmail.com>
2022-04-04 09:29:50 +0900

    not null制約前まで

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,11 @@ class TasksController < ApplicationController
   
   def index
     @tasks = Task.all
+    
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,6 +20,19 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}を更新しました。」"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
